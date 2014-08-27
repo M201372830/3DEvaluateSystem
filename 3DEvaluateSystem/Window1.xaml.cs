c30b5d539267cebb8502cc79f4dd04aea9b11860
SJQ <sjq597@gmail.com>
2014-08-27 13:28:36 +0800

    3DEvaluateSystem
    第一次初始化提交

---
+++ 3DEvaluateSystem/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿/**
+ *	FUNCTION 3D软件评估系统
+ *	TIME     :2014-8-27   13:05
+ *	AUTHOR   :SJQ				
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3DEvaluateSystem
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+﻿# 3D软件评估系统
+## 3DEvaluateSystem
+&emsp;&emsp;主项目，代码用WPF编写
+
+## Test
+&emsp;&emsp;学习使用WPF，数据库，小的Demo样例
